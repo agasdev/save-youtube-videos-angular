221ad86ae8074751b7c107449c1248d5b916f0fe
Andrés Gas <andresgasdev@gmail.com>
2020-08-16 17:39:18 +0200

    Add main components and routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'error', component: ErrorComponent},
+  {path: '**', component: ErrorComponent},
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
